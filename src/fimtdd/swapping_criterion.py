aa02c730f5800a47a303fa72b62e5ce1e76da1ae
MLRichter <DuL160779!>
2021-04-05 18:32:05 +0200

    proceeding with implemtation

--- src/fimtdd/swapping_criterion.py
+++ src/fimtdd/swapping_criterion.py
@@ -1,7 +1,22 @@
 from .domain import SwappingCriterion
 from attr import attrs, attrib
+import numpy as np
 
 
-@attrs(auto_attribs=True, frozen=True, slots=True)
+@attrs(auto_attribs=True, slots=True)
 class DecayingSquaredErrorSwapper:
-    ...
+
+    decay: float = 0.995
+    check_intervall: int = 50
+    _samples: int = 0
+    _cumulative_squared_error: float = 0.0
+    _cumulative_squared_error_alt: float = 0.0
+
+    def update(self, y: float, y_pred: float, alt_y_pred: float) -> bool:
+        self._samples += 1
+        self._cumulative_squared_error = (self._cumulative_squared_error*self.decay) + ((y-y_pred)**2)
+        self._cumulative_squared_error_alt = (self._cumulative_squared_error_alt*self.decay) + ((y-alt_y_pred)**2)
+        if self._samples == 0 or self._samples%self.check_intervall != 0:
+            return False
+        else:
+            return np.log(self._cumulative_squared_error / self._cumulative_squared_error_alt) > 0
